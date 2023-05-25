6e003f8008975f21f012ffdc00fd05e3cbb148b3
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-05-25 22:04:51 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+  'spacy'
+]
+
+
+setuptools.setup(
+    name="NLIGraphSpacy",
+    version="0.2",
+    author="Vishnu Nandakumar",
+    author_email="nkumarvishnu25@gmail.com",
+    description="Knowledge graph using Spacy NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/Vishnunkumar/NLIGraphSpacy',
+    packages=[
+        'NLIGraphSpacy',
+    ],
+    package_dir={'NLIGraphSpacy': 'NLIGraphSpacy'},
+    package_data={
+        'NLIGraphSpacy': ['*']
+    },
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='NLIGraphSpacy',
+    classifiers=(
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    ),
+)
