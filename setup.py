914135bea48aab7232bfdb108733c5002b89eb04
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-06-22 17:12:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = [
 
 setuptools.setup(
     name="nligraphspacy",
-    version="0.1.2",
+    version="1.1.0",
     author="Vishnu Nandakumar",
     author_email="nkumarvishnu25@gmail.com",
     description="Knowledge graph using Spacy NLP",
